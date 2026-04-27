494bd020defbdf65c2f72c4ce058058b3d170b7f
James28Dev <James28.Dev@gmail.com>
2026-04-27 23:40:47 +0700

    feat: enable nix store optimization and automatic garbage collection

--- modules/core.nix
+++ modules/core.nix
@@ -6,6 +6,11 @@
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.systemd-boot.enable = true;
 
+  nix.settings.auto-optimise-store = true;
+  nix.gc.automatic = true;
+  nix.gc.dates = "weekly";
+  nix.gc.options = "--delete-older-than 14d";
+
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "th_TH.UTF-8/UTF-8" ];
 
