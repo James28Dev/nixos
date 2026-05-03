696438d88dc429275ad7d9450cf9c43551556a2d
James28Dev <James28.Dev@gmail.com>
2026-05-04 00:55:54 +0700

    refactor: remove networkmanager from core module

--- modules/core.nix
+++ modules/core.nix
@@ -14,8 +14,6 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "th_TH.UTF-8/UTF-8" ];
-
-  networking.networkmanager.enable = true;
   time.timeZone = "Asia/Bangkok";
 
   # --- System-wide Fonts ---
