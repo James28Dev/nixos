65542b76c9a238b84bf2038fad11bb022b8a9a9b
James28Dev <James28.Dev@gmail.com>
2026-05-04 13:06:19 +0700

    feat: add multi-machine support for PC and ROG G14 configurations

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "James's NixOS Flake Configuration";
+  description = "James's Multi-Machine NixOS Flake";
 
   # --- External Resources & Libraries ---
   inputs = {
@@ -8,12 +8,26 @@
 
   # --- System Output Definitions ---
   outputs = { self, nixpkgs, ... } @inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-      ];
-      specialArgs = { inherit inputs; };
-      system = "x86_64-linux";
+    nixosConfigurations = {
+      # --- สำหรับเครื่อง PC (GTX 1050) ---
+      pc = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+          ./modules/hardware/nvidia-gtx1050.nix # โหลดเฉพาะตัว PC
+        ];
+        specialArgs = { inherit inputs; };
+        system = "x86_64-linux";
+      };
+
+      # --- สำหรับเครื่อง ROG G14 (GTX 1650 Ti Hybrid) ---
+      rog = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+          ./modules/hardware/nvidia-gtx1650ti-g14-hybrid.nix # โหลดเฉพาะตัว G14
+        ];
+        specialArgs = { inherit inputs; };
+        system = "x86_64-linux";
+      };
     };
   };
 }
